1cc5411f55e400aae54e81b4b3122e52f06b8977
97kitt <aMoore@mooredevelopments.com>
2016-09-15 22:23:00 -0500

    Add files via upload

---
+++ Dispatch/User.swift
@@ -0,0 +1,31 @@
+//
+//  User.swift
+//  Dispatch
+//
+//  Created by MooreDev on 8/28/16.
+//  Copyright © 2016 MooreDevelopments. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    
+    var Name: String?
+    var eMail: String?
+    var last_online: NSDate?
+    var online: String?
+    
+    
+    //var name: String?
+    var Adress: String?
+    var Zone_Area: String?
+    var Job_Status: String?
+    var Job_Type: String?
+    var MC:String?
+    var Notes:String?
+    var PO_Number:String?
+    var Phone1: String?
+    var Phone2: String?
+    var Time_Logged: String?
+    var Vehicle: String?
+}
